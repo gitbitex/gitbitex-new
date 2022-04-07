43e7c39fd5a9548a752e48e33bde75d160f57c7f
hahaha <lingqing.wan@live.com>
2022-04-08 00:33:35 +0800

    fix static url 404

--- src/main/java/com/gitbitex/restserver/controller/HomeController.java
+++ src/main/java/com/gitbitex/restserver/controller/HomeController.java
@@ -4,8 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HomeControlelr {
-    @GetMapping(value = {"/trade/BTC-USDT", "account/*"})
+public class HomeController {
+    @GetMapping(value = {"trade/*", "account/*"})
     public String test() {
         return "forward:/index.html";
     }
