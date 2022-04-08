bbac841c66feca4a319f90742834afe13166b5e4
lingqing.wan <lingqing.wan@alibaba-inc.com>
2022-04-08 15:56:14 +0800

    reformat

--- src/main/java/com/gitbitex/AppConfiguration.java
+++ src/main/java/com/gitbitex/AppConfiguration.java
@@ -14,7 +14,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @RequiredArgsConstructor
 @EnableConfigurationProperties(AppProperties.class)
-public class AppConfig {
+public class AppConfiguration {
 
     @Bean
     public KafkaMessageProducer kafkaMessageProducer(AppProperties appProperties, KafkaProperties kafkaProperties) {
